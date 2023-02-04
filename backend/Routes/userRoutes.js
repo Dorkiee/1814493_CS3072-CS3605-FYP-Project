b430976865360053164f459ba88bd6dfb9d8565b
Dorkiee <ma19kdo@gmail.com>
2023-02-04 19:16:14 +0000

    creating routes and authentication

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,13 @@
+
+import express from 'express'
+import userController from '../Controllers/userController.js'
+const userRoutes = express.Router()
+
+userRoutes.post('/sign-up', userController)
+userRoutes.post('/sign-up-admin', userController)
+userRoutes.post('/log-in', userController)
+userRoutes.get('/log-in', userController) //change to get??
+
+
+
+export default userRoutes;
