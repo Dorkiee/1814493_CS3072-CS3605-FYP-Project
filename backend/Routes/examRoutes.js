be9c8298f71a438cd062f4091816946ee987f476
Dorkiee <ma19kdo@gmail.com>
2023-03-07 18:32:01 +0000

    fix 2

--- backend/Routes/examRoutes.js
+++ backend/Routes/examRoutes.js
@@ -2,8 +2,8 @@
 import express from 'express'
 import path from 'path'
 
-import examController from path.join(__dirname, '../controllers/examController.js')
-import userController from path.join(__dirname, '../controllers/userController.js')
+import examController from path.join(__dirname, "../controllers/examController.js")
+import userController from path.join(__dirname, "../controllers/userController.js")
 
 
 const examRoutes = express.Router() //add comment to include courseRoutes i think should work???
