25659f7ff85083e389a920c044e56fc63e3c92f3
Dorkiee <ma19kdo@gmail.com>
2023-03-08 21:46:18 +0000

    update alert

--- frontend/src/employee_pages/signup.js
+++ frontend/src/employee_pages/signup.js
@@ -76,9 +76,7 @@ class signup extends Component {
       axios
         .post('https://phishshield-1814493.onrender.com/app/sign-up', registered)
         .then((data) => {
-          if (data.status === "ok") {
           alert('Congratulations! Your sign up was successful. Please proceed to the login page to access your account.');
-          }
         })
         .catch(error => console.error(error));
     } else {
